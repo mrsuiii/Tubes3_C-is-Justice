60cf9c66e91b468cfa1e337fded9f585bee31130
mrsuiii <emeryfathanzwageri@gmail.com>
2024-05-20 19:13:58 +0700

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Resources;
+using System.Windows.Shapes;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Search(object sender, RoutedEventArgs e) { 
+        }
+
+        private void ChooseCitra(object sender, RoutedEventArgs e) { }
+        
+
+        private void ToggleButton_Algoritma_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_SelectImage(object sender, RoutedEventArgs e)
+        {
+            // Handle select image click
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImagePlaceholder.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+
+    }
+}

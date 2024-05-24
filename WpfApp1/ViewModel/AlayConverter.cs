75674f2cdc7b0c6b2682787f2fcf56f6c2abb5e3
Devinzenzhang <devinzenzhang@gmail.com>
2024-05-24 22:04:23 +0700

    regex converter nama alay (blm string matching)

---
+++ WpfApp1/ViewModel/AlayConverter.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class AlayConverter {
+    static string AlayTransform(string namaAlay){
+        // mengubah semua angka alay menjadi huruf
+        Dictionary<char, char> digitToLetterMap = new Dictionary<char, char>
+        {
+            { '0', 'o' },
+            { '1', 'i' },
+            { '2', 'z' },
+            { '3', 'e' },
+            { '4', 'a' },
+            { '5', 's' },
+            { '6', 'g' },
+            { '7', 't' },
+            { '8', 'b' },
+            { '9', 'g' }
+        };
+        string angkaAlay = "[0123456789]";
+        string converted = Regex.Replace(namaAlay, angkaAlay, match => {return digitToLetterMap[match.Value[0]].ToString();});
+
+        // mengubah semua huruf besar menjadi huruf kecil
+        string hurufBesarAlay = "[A-Z]";
+        converted = Regex.Replace(converted, hurufBesarAlay, match => match.Value.ToLower());
+
+        // idk apakah penyingkatan dan spasi perlu dihandle
+
+        return converted;
+    }
+}
